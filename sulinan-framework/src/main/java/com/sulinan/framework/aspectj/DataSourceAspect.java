c38a123b5ac4008aaf6bd7432f4148d5af937629
zhangyu <2016462316@qq.com>
2026-06-05 15:39:58 +0800

    chore(config): 将项目包名从 ruoyi 替换为 sulinan
    - 更新 ApplicationConfig 中的 MapperScan 注解包路径
    - 修改 CaptchaConfig 中的 KaptchaTextCreator 类路径
    - 替换 Constants 常量中的包名白名单配置
    - 更新 DataSourceAspect 中的注解包路径引用
    - 更改 logback.xml 中的日志存放路径

--- sulinan-framework/src/main/java/com/sulinan/framework/aspectj/DataSourceAspect.java
+++ sulinan-framework/src/main/java/com/sulinan/framework/aspectj/DataSourceAspect.java
@@ -27,8 +27,8 @@ public class DataSourceAspect
 {
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Pointcut("@annotation(com.ruoyi.common.annotation.DataSource)"
-            + "|| @within(com.ruoyi.common.annotation.DataSource)")
+    @Pointcut("@annotation(com.sulinan.common.annotation.DataSource)"
+            + "|| @within(com.sulinan.common.annotation.DataSource)")
     public void dsPointCut()
     {
 
